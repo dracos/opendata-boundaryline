958566d0fcc2c27eb759bedbe6a647a8e11de932
Matthew Somerville <matthew-github@dracos.co.uk>
2010-04-06 02:21:44 +0100

    Debug static, make sure admin is first in urls list.

--- urls.py
+++ urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^', include('boundaryline.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^', include('boundaryline.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT
+        })
+    )
+
