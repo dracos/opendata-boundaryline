439f57f583becdc1c9f0ad756393978cb7f2e43e
Matthew Somerville <matthew-github@dracos.co.uk>
2010-04-09 10:10:31 +0100

    Add point lookup.

--- boundaryline/urls.py
+++ boundaryline/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('boundaryline.views',
     (r'^wkt(?:/(?P<snac>.*?))$', 'wkt'),
+    (r'^(?P<easting>\d+),(?P<northing>\d+)$', 'point'),
     (r'^brum$', 'brum'),
     (r'^(?:(?P<snac>.*?))$', 'home'),
 )
--- boundaryline/views.py
+++ boundaryline/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404
+from django.contrib.gis.geos import Point
 
 from models import Council
 
@@ -33,4 +34,13 @@ def wkt(request, snac='00CN'):
         raise Http404
     c.shape.transform(900913)
     return HttpResponse(c.shape.wkt, content_type='text/plain')
+
+def point(request, easting, northing):
+    point = Point(int(easting), int(northing), srid=27700)
+    c = Council.objects.filter(shape__contains=point).exclude(code='999999')
+    if not c:
+        raise Http404
+    return render_to_response('council.html', {
+        'council': c[0],
+    })
 
