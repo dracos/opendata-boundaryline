a51d1de4c587146c87461c99bfc3ca435190ea95
Matthew Somerville <matthew-github@dracos.co.uk>
2013-03-16 14:15:17 +0000

    Upgrade to Django 1.5.

--- boundaryline/urls.py
+++ boundaryline/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns
 
 urlpatterns = patterns('boundaryline.views',
     (r'^wkt(?:/(?P<snac>.*?))$', 'wkt'),
---
+++ project/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^', include('boundaryline.urls')),
+)
+
+urlpatterns += staticfiles_urlpatterns()
